ea14cbd2cfbb769c2611910f334f373b5a802dfd
Geno <geno@fireorbit.de>
2015-07-17 19:30:13 +0200

    fetch api info

--- server/api/monitormap/fetch.js
+++ server/api/monitormap/fetch.js
@@ -6,10 +6,8 @@ module.exports = function(data,res,next,io) {
 
 	if(data.passphrase == config.passphrase){
 		var mac = data.mac.slice(0, 2)+':'+data.mac.slice(2, 4)+':'+data.mac.slice(4, 6)+':'+data.mac.slice(6, 8)+':'+data.mac.slice(8, 10)+':'+data.mac.slice(10, 12);
-		console.log(mac);
 		models.Node.findOne({where:{mac:mac}}).then(function(cur_node){
 			//var cur_node = node[0];
-			console.log('in Node');
 			var tmp = {
 				channel_24:data.radio24_channel,
 				channel_50:data.radio5_channel,
@@ -19,13 +17,11 @@ module.exports = function(data,res,next,io) {
 			if(cur_node){
 				cur_node.updateAttributes(tmp).then(function(b){
 					res.status(200).send('ok').end();
-					console.log('a');
 				});
 			}else{
 				res.status(403).send('failed').end();
 			}
 		});
-		console.log('end');
 	}else{
 		res.status(403).send('failed').end();
 	}
