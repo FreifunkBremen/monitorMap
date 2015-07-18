8ceb5e7d5be3e18b79d7e320c1a920ca84dd2288
Geno <geno@fireorbit.de>
2015-07-18 22:30:52 +0200

    Updates

--- server/components/ansible.js
+++ server/components/ansible.js
@@ -50,23 +50,25 @@ module.exports.getJSON = function(fn){
     };
     for(var i in nodes){
       var node = nodes[i]
-      var name = node.mac.replace(/[:-]/g,"");
-      var mac  = node.mac.replace(/:/g,"-").split("-");
-      if(node.name.indexOf("CPE")==-1)
-        mac[5] = dec2hex(hex2dec(mac[5])-2);
+      if(node.status){
+        var name = node.mac.replace(/[:-]/g,"");
+        var mac  = node.mac.replace(/:/g,"-").split("-");
+        if(node.name.indexOf("CPE")==-1)
+          mac[5] = dec2hex(hex2dec(mac[5])-2);
 
-      output.nodes.push(name);
+        output.nodes.push(name);
 
-      output._meta.hostvars[name] = {
-        "ansible_ssh_host": ipv6calc.toIPv6(config.scanner.ipv6_prefix, mac.join(':')),
-        "node_name":        node.name,
-        "radio24_channel":  node.channel_24,
-        "radio24_txpower":  node.channel_24_power,
-        "radio5_channel":   node.channel_50,
-        "radio5_txpower":   node.channel_50_power,
-        "geo_latitude":     node.lat,
-        "geo_longitude":    node.lon
-      };
+        output._meta.hostvars[name] = {
+          "ansible_ssh_host": ipv6calc.toIPv6(config.scanner.ipv6_prefix, mac.join(':')),
+          "node_name":        node.name,
+          "radio24_channel":  node.channel_24,
+          "radio24_txpower":  node.channel_24_power,
+          "radio5_channel":   node.channel_50,
+          "radio5_txpower":   node.channel_50_power,
+          "geo_latitude":     node.lat,
+          "geo_longitude":    node.lon
+        };
+      }
     }
     if(!fn)
       return output;
